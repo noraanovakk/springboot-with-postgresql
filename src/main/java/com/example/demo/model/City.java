04e298a8e64bbbec7f7dd3ad297251b939601c82
noraanovakk <noraanovakk@gmail.com>
2022-09-01 10:41:57 +0200

    add gitignore and City entity

---
+++ src/main/java/com/example/demo/model/City.java
@@ -0,0 +1,53 @@
+package com.example.demo.model;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import javax.persistence.*;
+import java.util.Objects;
+
+@Data
+@Entity
+@Table(name = "cities")
+@NoArgsConstructor
+@AllArgsConstructor
+public class City {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.SEQUENCE)
+    private long id;
+
+    private String name;
+
+    private int population;
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj) {
+            return true;
+        }
+        if (obj == null) {
+            return false;
+        }
+        if (getClass() != obj.getClass()) {
+            return false;
+        }
+        final City other = (City) obj;
+        if (this.population != other.population) {
+            return false;
+        }
+        if (!Objects.equals(this.name, other.name)) {
+            return false;
+        }
+        return Objects.equals(this.id, other.id);
+    }
+
+    @Override
+    public String toString() {
+        return "City{" + "id=" + id +
+                ", name='" + name + '\'' +
+                ", population=" + population +
+                '}';
+    }
+}
